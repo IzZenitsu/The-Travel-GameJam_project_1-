e18bc821b9b6ccae1a81250af95674f706945930
Rishi-Pandey07 <bandananfr@gmail.com>
2026-06-22 11:28:00 +0530

    ThrowingScript modified and SampleScene modified

--- The Travel/Assets/GameFiles/Scripts/PlayerScript/ThrowingScript.cs
+++ The Travel/Assets/GameFiles/Scripts/PlayerScript/ThrowingScript.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThrowingScript : MonoBehaviour
 {
     public Transform PickUpSlot;
+    public GameObject FBackground;
+    
     void Update()
     { 
         if(PickUpSlot.childCount == 1)
         {
+            FBackground.SetActive(true);
             if (Input.GetKeyDown(KeyCode.F))
             {
                 GameObject child = PickUpSlot.GetChild(0).gameObject;
@@ -14,6 +18,10 @@ public class ThrowingScript : MonoBehaviour
                 child.transform.SetParent(null);
                 child.GetComponent<Rigidbody>().useGravity = true;              
             }
+        }
+        else
+        {
+            FBackground.SetActive(false);
         }
     }
 }
